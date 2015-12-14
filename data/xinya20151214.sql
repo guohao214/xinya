8f97fb020f739f2232a943019c4e3b9d47bc378c
GuoHao <guohao214@gmail.com>
2015-12-14 19:09:58 +0800

    datebase

--- data/xinya20151214.sql
+++ data/xinya20151214.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50612
 File Encoding         : 65001
 
-Date: 2015-12-14 18:59:52
+Date: 2015-12-14 19:09:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,6 +32,23 @@ CREATE TABLE `category` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `info`
+-- ----------------------------
+DROP TABLE IF EXISTS `info`;
+CREATE TABLE `info` (
+  `info_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL COMMENT '内容',
+  `type` varchar(30) NOT NULL COMMENT '内容类型',
+  `disabled` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`info_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='信息表(公司简介.关于我们...)';
+
+-- ----------------------------
+-- Records of info
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for `order`
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
@@ -42,6 +59,9 @@ CREATE TABLE `order` (
   `prepay_id` char(64) DEFAULT NULL COMMENT '预支付ID',
   `transaction_id` char(32) DEFAULT NULL COMMENT '微信订单号',
   `total_fee` float(10,2) unsigned NOT NULL COMMENT '订单金额',
+  `pay_status` tinyint(2) unsigned NOT NULL COMMENT '订单状态',
+  `pay_time` datetime DEFAULT NULL COMMENT '订单支付时间',
+  `complete_time` datetime DEFAULT NULL COMMENT '订单完成时间（到店内进行了美容)',
   `create_time` datetime NOT NULL COMMENT '下单时间',
   PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -51,11 +71,29 @@ CREATE TABLE `order` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `order_project`
+-- ----------------------------
+DROP TABLE IF EXISTS `order_project`;
+CREATE TABLE `order_project` (
+  `order_project_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(11) unsigned NOT NULL,
+  `project_id` bigint(11) unsigned NOT NULL,
+  `fee` float(10,2) unsigned NOT NULL COMMENT '项目金额',
+  `buy_counts` int(11) unsigned NOT NULL DEFAULT '1' COMMENT '项目购买数量',
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`order_project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单下的项目表';
+
+-- ----------------------------
+-- Records of order_project
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for `project`
 -- ----------------------------
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
-  `project_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '项目ID',
+  `project_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '项目ID',
   `project_name` varchar(100) NOT NULL COMMENT '项目名称',
   `categoty_id` int(11) unsigned NOT NULL COMMENT '所属分类',
   `project_cover` varchar(500) DEFAULT NULL COMMENT '封面',
