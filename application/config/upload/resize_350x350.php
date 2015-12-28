72bc2f3dfb660f067e127ed16fa0086f82a8cd56
GuoHao <guohao214@gmail.com>
2015-12-28 13:56:39 +0800

    开发

---
+++ application/config/upload/resize_350x350.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ * User: Administrator
+ * Date: 15-12-15
+ * Time: 下午7:10
+ */
+
+return array(
+    'image_library' => 'gd2',
+    'create_thumb' => TRUE,
+    'maintain_ratio' => TRUE,
+    'width' => 350,
+    'height' => 350,
+    'thumb_marker' => '_350x350'
+);
