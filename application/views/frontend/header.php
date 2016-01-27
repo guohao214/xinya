d06e9839369a5f0d9fa785615eac814bbb75c597
GuoHao <guohao214@gmail.com>
2016-01-27 11:47:38 +0800

    去除禁止缓存

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -5,9 +5,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
     <link rel="stylesheet" href="<?php echo get_instance()->config->base_url(); ?>static/frontend/css/css.css?v=20150117010360111">
     <link rel="stylesheet" href="<?php echo get_instance()->config->base_url(); ?>static/frontend/css/default.css">
     <link rel="stylesheet" href="<?php echo get_instance()->config->base_url(); ?>static/frontend/css/loading.css?v=20160113">
@@ -24,5 +21,11 @@
          width="60" height="60" align="absmiddle"/>'
 </div>
 
+<!--
+    禁止缓存
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+--!>
 
 <div id="message" align="absmiddle">操作完成</div>
